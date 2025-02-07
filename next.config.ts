fcf18e2d2208cc01e39717b70b4f2a950b8040dc
student9876 <shouvik9876@gmail.com>
2025-02-07 15:44:28 +0530

    Add backend URL in domain next config

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['localhost', 'res.cloudinary.com', 'render.com', `${process.env.BACKEND_URL}`],
+    domains: ["localhost", "res.cloudinary.com", "https://chat-app-backend-a2im.onrender.com"],
   },
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
     FRONTEND_URL: process.env.FRONTEND_URL
   }
 };
-
 
 export default nextConfig;
